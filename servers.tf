7e18fb4aab51de4c2c98705768eee0194b93112a
sunil <sunilkumar.ipm@gmail.com>
2023-05-16 15:54:40 +0530

    changes

--- servers.tf
+++ servers.tf
@@ -4,9 +4,8 @@ data "aws_ami" "centos" {
   name_regex  = "Centos-8-DevOps-Practice"
 }
 
-
-data "aws_security_group" "allow-all" {
-  name = "allow-all"
+data "aws_security_group" "allow_all" {
+  name = "allow_all"
 }
 
 variable "instance_type" {
@@ -40,7 +39,7 @@ resource "aws_instance" "instance" {
   for_each         = var.components
   ami           = data.aws_ami.centos.image_id
   instance_type = each.value ["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  vpc_security_group_ids = [data.aws_security_group.allow_all.id]
 
   tags = {
     Name = each.value["name"]
@@ -50,7 +49,7 @@ resource "aws_instance" "instance" {
 
 resource "aws_route53_record" "records" {
   for_each = var.components
-  zone_id = "Z07723241NASCD733XBTT"
+  zone_id = "Z02273062C88PHAGX2U2W"
   name    = "${each.value["name"]}.devops2023sk.online"
   type    = "A"
   ttl     = 30
