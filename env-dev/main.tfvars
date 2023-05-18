fc5463ccce27eb20417d1fa2c73524873ec8c7cb
sunil <sunilkumar.ipm@gmail.com>
2023-05-18 16:59:23 +0530

    changes

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -13,10 +13,37 @@ components = {
     name = "catalogue"
     instance_type = "t3.small"
   }
-  payment = {
-    name          = "payment"
+    redis = {
+    name          = "redis"
     instance_type = "t3.small"
   }
+    user = {
+    name          = "user"
+    instance_type = "t3.micro"
+  }
+    cart = {
+    name          = "cart"
+    instance_type = "t3.micro"
+  }
+    mysql = {
+    name          = "mysql"
+    instance_type = "t3.small"
+    password = "RoboShop@1"
+  }
+    shipping = {
+    name          = "shipping"
+    instance_type = "t3.medium"
+    password = "RoboShop@1"
+  }
+    rabbitmq = {
+    name          = "rabbitmq"
+    instance_type = "t3.small"
+    password = "roboshop123"
+  }
+    payment = {
+    name          = "payment"
+    instance_type = "t3.small"
+    password = "roboshop123"
+  }
 }
-
 env = "dev"
--- servers.tf
+++ servers.tf
@@ -5,15 +5,39 @@ variable "instance_type" {
 }
 
 resource "aws_instance" "instance" {
-  for_each         = var.components
-  ami           = data.aws_ami.centos.image_id
-  instance_type = each.value ["instance_type"]
+  for_each               = var.components
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = each.value ["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow_all.id]
 
   tags = {
     Name = each.value["name"]
   }
+
 }
+
+resource "null_resource" "provisioner"  {
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+    for_each               = var.components
+    provisioner "remote-exec" {
+      connection {
+        type     = "ssh"
+        user     = "centos"
+        password = "DevOps321"
+        host     = [aws_instance.instance[each.value["name"]].private_ip]
+      }
+
+      inline = [
+        "rm -rf Roboshop-shell",
+        "git clone https://github.com/sunil824922/roboshop-shell.git",
+        "sudo bash ${each.value["name"]}.sh ${lookup(each.value, "password", "null") }"
+      ]
+    }
+  }
+
+
+
+
 
 
 resource "aws_route53_record" "records" {
