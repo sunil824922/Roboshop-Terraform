81237fad8232460ab2fb4d4d4caefda3258df44d
sunil <sunilkumar.ipm@gmail.com>
2023-06-01 19:10:45 +0530

    iam role terraform

--- module/main.tf
+++ module/main.tf
@@ -58,3 +58,36 @@ resource "aws_iam_role" "role" {
     tag-key = "${var.component_name}-${var.env}-role"
   }
 }
+
+
+resource "aws_iam_role_policy" "ssm-ps-policy" {
+  name = "${var.component_name}-${var.env}-ssm-ps-policy"
+  role = aws_iam_role.role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "VisualEditor0",
+        "Effect": "Allow",
+        "Action": [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource": "arn:aws:ssm:us-east-1:116420554906:parameter/${var.env}.${var.component_name}*"
+      },
+      {
+        "Sid": "VisualEditor1",
+        "Effect": "Allow",
+        "Action": "ssm:DescribeParameters",
+        "Resource": "*"
+      }
+    ]
+  })
+}
+
+
