16cf2f8a08e26c5c99e076736bb518c42162c8cf
sunil <sunilkumar.ipm@gmail.com>
2023-07-05 18:37:56 +0530

    prometheus

--- module/locals.tf
+++ module/locals.tf
@@ -11,8 +11,13 @@ locals {
   app_commands = [
      "sudo labauto ansible",
      "ansible-pull -i localhost, -U https://github.com/sunil824922/roboshop-ansible.git roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
-
   ]
+  db_tags = {
+    Name= "${var.component_name}-${var.env}"
+  }
+  app_tags = {
+    Name= "${var.component_name}-${var.env}"
+  }
 }
 
 
