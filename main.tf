659f080a3a515dce69e5c4a6b6e284222a315791
sunil <sunilkumar.ipm@gmail.com>
2023-07-17 17:42:43 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "vpc" {
-  source = "https://github.com/sunil824922/tf-module-vpc.git"
+  source = "git::https://github.com/sunil824922/tf-module-vpc.git"
 
   for_each = var.vpc
   cidr_block = each.value["cide_block"]
 }
+
