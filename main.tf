b0a12f0c9179bd9005775d20d72c82b0e17da3de
sunil <sunilkumar.ipm@gmail.com>
2023-07-18 11:37:32 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
   source = "git::https://github.com/sunil824922/tf-module-vpc.git"
 
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value["cidr_block"]
-  tags = var.tags
+  tags       = local.tags
 }
 
--- tags.tf
+++ tags.tf
@@ -1,8 +1,10 @@
-tags = {
-    business_unit = "ecommerce"
-    business_type = "retail"
-    project = "roboshop"
-    cost_center = 100
-    env = var.env
-  }
+locals {
+    tags = {
+        business_unit = "ecommerce"
+        business_type = "retail"
+        project = "roboshop"
+        cost_center = 100
+        env = var.env
+    }
+}
 
