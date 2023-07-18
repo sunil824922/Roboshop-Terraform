80f90d535169437366d7f1c67380851bd550f0e4
sunil <sunilkumar.ipm@gmail.com>
2023-07-18 11:31:55 +0530

    vpc

--- main.tf
+++ main.tf
@@ -3,5 +3,6 @@ module "vpc" {
 
   for_each = var.vpc
   cidr_block = each.value["cidr_block"]
+  tags = var.tags
 }
 
---
+++ tags..tf
@@ -0,0 +1,8 @@
+tags = {
+    business_unit = "ecommerce"
+    business_type = "retail"
+    project = "roboshop"
+    cost_center = 100
+    env = var.env
+  }
+
