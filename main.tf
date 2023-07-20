28171f6240d7bd95cf4c8ce724395944dd53c556
sunil <sunilkumar.ipm@gmail.com>
2023-07-20 18:28:29 +0530

    auto scale-instance

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "vpc" {
 }
 
 
-module "web" {
+module "app" {
   source = "git::https://github.com/sunil824922/tf-module-app.git"
 
   for_each      = var.app
@@ -22,9 +22,9 @@ module "web" {
   env = var.env
   bastion_cidr = var.bastion_cidr
 
-  subnet_ids     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   vpc_id        = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
-  allow_app_cidr = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidr", null), 0)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
 
 }
 
