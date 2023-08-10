e566d37e3fc6b768dd5237e6390bab6814aaac65
sunil <sunilkumar.ipm@gmail.com>
2023-08-10 17:18:55 +0530

    parameters

--- vars.tf
+++ vars.tf
@@ -2,6 +2,7 @@ variable "env" {}
 variable "vpc" {}
 variable "app" {}
 variable "bastion_cidr" {}
+variable "monitor_cidr" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_vpc_rtid" {}
